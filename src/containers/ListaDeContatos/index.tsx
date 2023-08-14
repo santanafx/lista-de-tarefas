3c9eadbd416b53d53860bc43050a4fe5b7871230
Lucas Santana <santanafx@hotmail.com>
2023-08-14 13:51:32 -0300

    style: adicionar contato

--- src/containers/ListaDeContatos/index.tsx
+++ src/containers/ListaDeContatos/index.tsx
@@ -1,3 +1,28 @@
+import { Titulo } from '../../styles'
+import { BotaoCustomEditar, BotaoCustomRemover, Card, Main } from './style'
+
 export const ListaDeContatos = () => {
-  return <main>Lista de Contatos</main>
+  return (
+    <Main>
+      <Titulo>Lista de contatos:</Titulo>
+      <Card>
+        <div>
+          <label htmlFor="">Nome</label>
+          <input type="text" disabled value={'teste'} />
+          <BotaoCustomEditar>Editar</BotaoCustomEditar>
+        </div>
+        <div>
+          <label htmlFor="">Email</label>
+          <input type="email" disabled value={'teste'} />
+          <BotaoCustomEditar>Editar</BotaoCustomEditar>
+        </div>
+        <div>
+          <label htmlFor="">Telefone</label>
+          <input type="tel" disabled value={'teste'} />
+          <BotaoCustomEditar>Editar</BotaoCustomEditar>
+        </div>
+      </Card>
+      <BotaoCustomRemover>Remover</BotaoCustomRemover>
+    </Main>
+  )
 }
